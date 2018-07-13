bda4b658546f4333253e366ed33e977fe79e4bcf
brilee8820 <brilee8820@gmail.com>
2018-07-13 17:42:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# dog.rb
+class Dog 
+
+  def bark 
+    puts "Woof!"
+  end
+
+  def sit 
+    puts "The Dog is sitting"
+  end 
+end 
+
+
+# module Bark 
+#   def bark(sound)
+#     puts "#{sound}"
+#   end
+  
+#   class Dog 
+#     include bark
+#   end 
+  
+#   sparky = Dog.new 
+#   sparky.bark("Woof!")
+   
+# module Sit 
+#   def sit(sit)
+#     puts "#{sit}"
+#   end 
+  
+#   class Sit 
+#     include sit 
+#   end 
+  
+#   sparky.sit("The Dog is sitting")
+# end
+    
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# person.rb
+class Person
+  
+  def talk 
+    puts "Hello World!"
+end
+
+  def walk 
+    puts "The Person is walking"
+  end 
+end 
+    
+
+# module Talk 
+#   include talk(sound)
+#     puts "#{talk}"
+# end
+  
+#   class Person
+#     incluse talk 
+#   end
+  
+#   katie = Person.new 
+#   katie = 
+
